52bfe1389bf65839c7d8ae7bbad9d05e1c8b1606
lnbatioco <LoryleenBatioco@gmail.com>
2018-01-01 20:35:51 -0800

    initial commit

---
+++ app/src/main/java/com/loryleen/notebook/MainActivity.java
@@ -0,0 +1,16 @@
+package com.loryleen.notebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+    }
+}
